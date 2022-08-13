342b3cbd9102d98b07544d0c60f48fdd436c4c4f
belkhiritaha <belkhiri.taha53@gmail.com>
2022-08-13 20:42:54 +0100

    generating 1080x1920 video with minecraft parkour playing in the background

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from turtle import width
 from gtts import gTTS
 import praw
 from bs4 import BeautifulSoup
@@ -7,6 +8,8 @@ from datetime import datetime
 import os
 import moviepy.editor as mpy
 from mutagen.mp3 import MP3
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import random
 
 # create class RedditAuthor
 class RedditAuthor:
@@ -17,7 +20,7 @@ class RedditAuthor:
 
 # create class RedditObject
 class RedditObject:
-    def __init__(self, topic : str, comments : list, id : str, upvotes : int, author, timestamp, nbComments, awards):
+    def __init__(self, topic : str, comments : list, id : str, upvotes : int, author, timestamp, nbComments, awards, isNSFW):
         self.id = id
         self.topic = topic
         self.comments = comments
@@ -26,6 +29,7 @@ class RedditObject:
         self.timestamp = timestamp
         self.nbComments = nbComments
         self.awards = awards
+        self.isNSFW = isNSFW
 
     def __str__(self):
         return self.topic + " with " + str(len(self.comments))
@@ -66,7 +70,7 @@ def getPosts(numberOfPosts):
                 author = RedditAuthor(top_level_comment.author.name, top_level_comment.author.icon_img)
                 comment = RedditComment(top_level_comment.body, author, top_level_comment.ups, top_level_comment.created_utc, top_level_comment)
                 commentList.append(comment)
-        post = RedditObject(submission.title, commentList, submission.id, submission.ups, submission.author.name, submission.created_utc, submission.num_comments, submission.all_awardings)
+        post = RedditObject(submission.title, commentList, submission.id, submission.ups, submission.author.name, submission.created_utc, submission.num_comments, submission.all_awardings, submission.over_18)
         postsList.append(post)
         print("added post")
     return postsList
@@ -74,7 +78,6 @@ def getPosts(numberOfPosts):
 
 def postToSpeech(post):
     directory = "./" + post.id + "/"
-    # set speed to high
     topic = gTTS(post.topic, lang="en", slow=False)
     #sox_effects = ("speed", "1.5")
     topic.save(directory + "topic.mp3")
@@ -147,8 +150,15 @@ def generateTopicScreenShot(post : RedditObject):
         awardsTag.append(awardTag)
         print("added award")
 
-    # write to html file
+    # if post is nsfw
+    if post.isNSFW:
+        topicContentTag = soup.find("div", attrs={"class": "topic-content"})
+        nsfwTag = soup.new_tag("p", attrs={"class": "nsfw float-child"})
+        nsfwTag.string = "nsfw"
+        topicContentTag.append(nsfwTag)
 
+
+    # write to html file
     with open(directory + "topicOutput.html", "w") as file:
         file.write(str(soup))
 
@@ -234,47 +244,68 @@ def createVideo(post : RedditObject):
     # get post directory
     directory = "./" + post.id + "/"
 
+    videoLength = 0
     videoFrames = []
 
     # get topic frame
     topicImage = directory + "topic.png"
     topicAudio = directory + "topic.mp3"
     topicDuration = getMP3AudioDuration(topicAudio)
-    frame = mpy.ImageClip(topicImage, duration=topicDuration)
+    frame = mpy.ImageClip(topicImage, duration=topicDuration + 1)
     frame = frame.set_audio(mpy.AudioFileClip(topicAudio))
     topicFrame = FrameObject(99, frame, topicImage, topicAudio, topicDuration)
     videoFrames.append(topicFrame)
 
+    videoLength += topicDuration
+
     # get comment frames
-    for i in range(len(post.comments)):
+    i = 0
+    while i < (len(post.comments)):
         commentImage = directory + "comment" + str(i) + ".png"
         commentAudio = directory + "comment" + str(i) + ".mp3"
         commentDuration = getMP3AudioDuration(commentAudio)
-        frame = mpy.ImageClip(commentImage, duration=commentDuration)
-        frame = frame.set_audio(mpy.AudioFileClip(commentAudio))
-        commentFrame = FrameObject(i, frame, commentImage, commentAudio, commentDuration)
-        videoFrames.append(commentFrame)
-
-
+        print("comment duration: " + str(commentDuration))
+        print("video length: " + str(videoLength))
+        if videoLength + commentDuration < 10:# or i == 0:        
+            frame = mpy.ImageClip(commentImage, duration=commentDuration + 1)
+            frame = frame.set_audio(mpy.AudioFileClip(commentAudio))
+            commentFrame = FrameObject(i, frame, commentImage, commentAudio, commentDuration)
+            videoFrames.append(commentFrame)
+            videoLength += commentDuration
+        i += 1
     video = mpy.concatenate_videoclips([x.imageClip for x in videoFrames], method="compose")
-    video.write_videofile(directory + "video.mp4", fps=24)
-    print("done")
+
+    rawBackground = mpy.VideoFileClip("../Downloads/backgroundVideo.mp4")
+    rawBackground = rawBackground.set_audio(None)
+    rawBackgroundDuration = rawBackground.duration
+
+    # get random subclip
+    randomStart = random.randint(30, int(rawBackgroundDuration) - int(video.duration) - 30)
+    backgroundVideo = rawBackground.subclip(randomStart, int(video.duration) + randomStart + 1)
+    backgroundVideo = backgroundVideo.resize((1080, 1920))
+
+    # center video
+    video = video.set_position("center")
+    # set new video dimensions
+    videoWidth = video.w
+    videoHeight = video.h
+    video = video.resize((videoWidth,videoHeight * 1.5))
+
+
+    # put video on top of background video
+    video = mpy.CompositeVideoClip([backgroundVideo, video])
+
+    video.write_videofile(directory + "video.mp4", fps=60)
+
 
 def main():
-    post = getPosts(2)[0]
-    generateTopicScreenShot(post)
-    for i in range(len(post.comments)):
-        generateCommentScreenShot(post.comments[i], i)
+    post = getPosts(1)[0]
+    #generateTopicScreenShot(post)
+    #for i in range(len(post.comments)):
+    #    generateCommentScreenShot(post.comments[i], i)
     
-    postToSpeech(post)
+    #postToSpeech(post)
     createVideo(post)
-
-    post2 = getPosts(2)[1]
-    generateTopicScreenShot(post2)
-    for i in range(len(post2.comments)):
-        generateCommentScreenShot(post2.comments[i], i)
     
-    postToSpeech(post2)
-    createVideo(post2)
 
 main()
